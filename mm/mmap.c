73c1a5f71deb2533cbf3e1d154ebb2498baaf368
dookiedude <vdmail@gmail.com>
2016-08-24 14:52:24 -0700

    Remove Samsung implementation of sdcardfs
    Remove Samsung version of sdcardfs before we use AOSP source
    
    Change-Id: Iea53d78ce7ea06bd3fcd00e662ef17028ba5a8cb

--- mm/mmap.c
+++ mm/mmap.c
@@ -37,9 +37,6 @@
 #include <asm/mmu_context.h>
 
 #include "internal.h"
-#ifdef CONFIG_SDCARD_FS
-#include "../fs/sdcardfs/sdcardfs.h"
-#endif
 
 #ifndef arch_mmap_check
 #define arch_mmap_check(addr, len, flags)	(0)
@@ -68,7 +65,7 @@ static void unmap_region(struct mm_struct *mm,
  * MAP_SHARED	r: (no) no	r: (yes) yes	r: (no) yes	r: (no) yes
  *		w: (no) no	w: (no) no	w: (yes) yes	w: (no) no
  *		x: (no) no	x: (no) yes	x: (no) yes	x: (yes) yes
- *		
+ *
  * MAP_PRIVATE	r: (no) no	r: (yes) yes	r: (no) yes	r: (no) yes
  *		w: (no) no	w: (no) no	w: (copy) copy	w: (no) no
  *		x: (no) no	x: (no) yes	x: (no) yes	x: (yes) yes
@@ -966,11 +963,6 @@ static unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 	int error;
 	unsigned long reqprot = prot;
 
-#ifdef CONFIG_SDCARD_FS
-	if (file && (file->f_path.mnt->mnt_sb->s_magic == SDCARDFS_SUPER_MAGIC))
-		file = sdcardfs_lower_file(file);
-#endif
-
 	/*
 	 * Does the application expect PROT_READ to imply PROT_EXEC?
 	 *
@@ -1389,10 +1381,10 @@ munmap_back:
 			free_page((unsigned long)tmp);
 			return PTR_ERR(pathname);
 		}
-		
-		if (strstr(pathname, "dalvik-heap") != NULL 
+
+		if (strstr(pathname, "dalvik-heap") != NULL
 				|| strstr(pathname, "dalvik-bitmap") != NULL
-				|| strstr(pathname, "dalvik-LinearAlloc") != NULL 
+				|| strstr(pathname, "dalvik-LinearAlloc") != NULL
 				|| strstr(pathname, "dalvik-mark-stack") != NULL
 				|| strstr(pathname, "dalvik-card-table") != NULL) {
 			//printk("PROC %s\tFILE %s\tSTART %lx\tLEN %lx\n", current->comm, pathname, addr, len);
@@ -1503,7 +1495,7 @@ full_search:
 		addr = vma->vm_end;
 	}
 }
-#endif	
+#endif
 
 void arch_unmap_area(struct mm_struct *mm, unsigned long addr)
 {
